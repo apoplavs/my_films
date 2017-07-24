66f9a2c76f2f3adc5824f262a5c439f9d000a2b7
apoplavs <p.andriy.v@gmail.com>
2017-07-24 13:13:25 +0300

    need to complete delete-film

---
+++ delete-films.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andriy
+ * Date: 23.07.17
+ * Time: 19:22
+ */
+
+if (!empty($_POST)) {
+    require_once ('ConnectDB.php');
+    $db = new ConnectDB();
+    foreach ($_POST as $id_film) {
+        $id = intval($id_film);
+        $actors = $db->get_result("SELECT `films_stars`.`star`
+        FROM `films_stars` WHERE `films_stars`.`film` = '$id'");
+        $db->change_data("DELETE FROM `db_films`.`films` WHERE `films`.`id` = '$id'");
+        if (!empty($actors)) {
+            foreach ($actors as $actor) {
+                $id_actor = intval($actor['star']);
+                $films_with_actor = $db->get_result("SELECT `films_stars`.`id`
+                FROM `films_stars` WHERE `films_stars`.`star` = '$id_actor'");
+                if (count($films_with_actor) < 2) {
+                    $db->change_data("DELETE FROM `db_films`.`stars` WHERE `stars`.`id` = '$id_actor'");
+                }
+                $db->change_data("DELETE FROM `db_films`.`films_stars`
+                WHERE `films_stars`.`star` = '$id_actor' AND `films_stars`.`film` = '$id'");
+            }
+        }
+    }
+}
+header("Location: index.php");
+?>
